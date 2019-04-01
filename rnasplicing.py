5bc89265745ce701ad06690e417bc469cecd4d65
rtanmay <rtanmay05@gmail.com>
2019-04-02 02:21:19 +0530

    Corrected the program :)

--- rnasplicing.py
+++ rnasplicing.py
@@ -1,80 +1,70 @@
+# This program only solves for non overlapping introns and only for certain combinations
+# E.g: removal of ana from banana. bna or ban (two combinations)
+############################################################################################
+# Importing the libraries
 from Bio.Seq import Seq
 from Bio import SeqIO
 
-#String to store the maturerna
-maturerna=""
+############################################################################################
+# Function to remove the introns in prerna
+def remove_intron(prernastr, intronstr):
+	index = 0
+	length = len(intronstr)
+	while prernastr.find(intronstr) != -1:
+		index = prernastr.find(intronstr)
+		prernastr = prernastr[0:index] + prernastr[index+length:]
+	return prernastr
 
-#Function to remove the introns in prerna
-def remove_intron(mainstring,substring):
-	global maturerna
-	maturerna=""
-	mainstring=str(mainstring)
-	substring=str(substring)
-	mlength=len(mainstring)
-	slength=len(substring)
-	i=0
-	while i<mlength:
-		initialpos=i
-		j=0
-		while i<mlength and j<slength and substring[j]==mainstring[i]:
-			i+=1
-			j+=1
-		j-=1
-		i-=1
-		if j==slength-1:
-			i=initialpos+j+1
-		else:
-			maturerna+=mainstring[i]
-			i=initialpos+1
-
+############################################################################################
 """
-The input is in FASTA format
-The first string is the DNA String
-The rest below them are introns string
+Input is in FASTA format
+First string is the DNA String
+Rest below them are the introns string
 """
 counter=0
-lis=[]
+intron_list=[]
 for record in SeqIO.parse("input.txt","fasta"):
-	if counter==0:
-		dna_string=record
+	if (counter==0):
+		dna_string=record.seq # record.seq returns an Seq object
 		counter+=1
 	else:
-		lis.append(str(record))
-
-"""
-Remove this when above starts working 
-
-#Input the DNA string
-dna_string=Seq(input())
-
-#Input the number of introns
-num_introns=int(input())
-i=0
-lis=[]
-while i<num_introns:
-	intron_string=input()
-	lis.append(intron_string)
-	i+=1
-"""
-
-
-#Get the rna string from dna
-prerna=dna_string.transcribe()
+		intron_list.append(str(record.seq))
+print("Reading Done!")
+countt=0
+print("DNA =",dna_string)
+for i in intron_list:
+	countt=countt+1
+	print("Intron %d = %s" % (countt,i))
+print("")
+prerna=dna_string
 prerna=str(prerna)
 
+############################################################################################
 """
 Removing the introns
 In case of overlapping introns in the prerna, the first occurence of intron is
 removed and then the subsequent string is considered for removal
 """
-for i in lis:
-	remove_intron(prerna,i) 
-	prerna=maturerna
-	maturerna=""
-maturerna=Seq(prerna)
-
-#Converting the matured rna to protein
-protein=str(maturerna.translate())
-
-#Output the protein formed
-print(protein)
+print("Removing the introns")
+for i in intron_list:
+	prerna=remove_intron(prerna,i) 
+print("Removal Done!")
+prerna=Seq(prerna)
+############################################################################################
+# Transcribtion
+print("Doing Transcribtion")
+prerna=prerna.transcribe()
+print("Transcribtion Done!")
+maturerna=prerna
+print("MaturedRNA = ",maturerna)
+print("MaturedRNA length = ",len(maturerna))
+print("")
+############################################################################################
+# Converting the matured rna to protein
+print("Translating the matured RNA")
+protein=maturerna.translate(to_stop=True)
+print("Translation Done!")
+############################################################################################
+# Output the protein formed
+print("Protein=",protein)
+############################################################################################
